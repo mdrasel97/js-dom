2afe743f56a11c8e0d07e8d1633cf1d714aa43a3
mdrasel97 <mollarasel972@gmail.com>
2025-02-22 15:00:59 +0600

    js practice

---
+++ app.js
@@ -0,0 +1,31 @@
+// const style = document.getElementById('hadding');
+// style.style.backgroundColor = 'red'
+
+// let title = document.getElementById('hadding');
+// title.innerText = 'Hello javascript';
+
+// let title = document.getElementById('hadding');
+// title.innerText = title.innerText + ' Rasel';
+
+
+// let boxes = document.querySelectorAll('.box');
+// boxes[0].innerText = 'new text';
+
+// let newNum = 1;
+// for(let box of boxes){
+//     box.innerText = `box No ${newNum}`;
+//     newNum++;
+// }
+
+
+// 1. const amar = document.getElementsByTagName('');
+
+// 2. const name = document.getElementById('');
+
+// 3. const nam = document.getElementsByClassName('');
+
+// 4. const nem = document.querySelector('');
+
+// 5. const nn = document.querySelectorAll('');
+
+
